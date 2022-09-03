6b26df78849b9ce69f04985382c0eb3e037dbe27
Marco Montorsi <marcomontorsi97@gmail.com>
2022-09-03 17:51:45 +0200

    DONE

--- dhclient.py
+++ dhclient.py
@@ -2,6 +2,7 @@
 import socket
 import random
 from Crypto.Cipher import AES
+import hashlib
 
 
 # Removes PKCS#7 padding 
@@ -11,61 +12,71 @@ def unpad(padded : bytes) -> bytes:
 	padlen= padded[size-1]
 	return padded[:size-padlen]
 
-
-#
-# NS-Lab-07, Exercise 3.
-#
 if __name__=="__main__":
 	server= "netsec.unipr.it"
-	port= 9001
+	port= 7022
 	
-	p= 171718397966129586011229151993178480901904202533705695869569760169920539808075437788747086722975900425740754301098468647941395164593810074170462799608062493021989285837416815548721035874378548121236050948528229416139585571568998066586304075565145536350296006867635076744949977849997684222020336013226588207303
-	g= 2
-	print("p:",p)
-	print("g:",g)
+	p_DH = 171718397966129586011229151993178480901904202533705695869569760169920539808075437788747086722975900425740754301098468647941395164593810074170462799608062493021989285837416815548721035874378548121236050948528229416139585571568998066586304075565145536350296006867635076744949977849997684222020336013226588207303
+	g_DH = 2
 
-	# choose a private value xc and compute yc
-	xc= random.getrandbits(512)
-	print("xc:",xc)
-	yc= pow(g,xc,p)
-	print("yc:",yc)
+	# choose a private key for DH and compute yClient = (g_DH ^ (xClient)) mod p_DH
+	xClient= random.getrandbits(512)
+	yClient= pow(g_DH,xClient,p_DH)
 	
-	# send client private value xc
-	print("connecting to",server,port)
+	# send client private value yClient
 	s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((server,port))
+
 	io= s.makefile("rw")
-	io.write(str(yc))
+	io.write('HELLO ' + str(yClient))
 	io.write("\r\n")
 	io.flush()
 	
-	# receive server public value ys
-	ys= int(io.readline())
-	print("ys:",yc)
-	
-	# compute DH secret
-	dh= pow(ys,xc,p)
-	print("dh:",dh)
+	# receive server public value yb
+	helloServerMessage = io.readline()
+
+	if(helloServerMessage[:6] != 'HELLO '):
+		print('Response Not Valid')
+		s.close()
+		exit()		
+
+	dh= pow(int(helloServerMessage[6:]), xClient, p_DH)
 	dh= dh.to_bytes(128,'big')
-	print("dh:",dh.hex())
-	
-	# receive ciphertext
-	hexdata= io.readline()
-	ciphertext= bytearray.fromhex(hexdata)
-	print("ciphertext:",ciphertext.hex())
+
+	serverCertificateMessage = io.readline()
+	if(serverCertificateMessage[:12] != 'CERTIFICATE '):
+		print(serverCertificateMessage[:12])
+		print('Response Not Valid')
+		s.close()
+		exit()	
+	serverCertificateVerifyMessage= io.readline()
+	if(serverCertificateVerifyMessage[:19] != 'CERTIFICATE_VERIFY '):
+		print('Response Not Valid')
+		s.close()
+		exit()	
+	serverFinishedMessage= io.readline()
+	if(serverFinishedMessage[:9] != 'FINISHED '):
+		print('Response Not Valid')
+		s.close()
+		exit()	
+
+	mac_s = bytes.fromhex(serverFinishedMessage[9:])
+	print('mac_s ' , mac_s)
+	print('secret' , dh[112:128])
+	mac_c =  dh[112:128] + mac_s
+	print('tot ', mac_c)
+	result = hashlib.sha256(mac_c)
+	print('response  ERROR Verification of the clinet MAC failed; expected value was ' + result.hexdigest())
+
+
+	io.write('FINISHED ' + result.hexdigest())
+	io.write("\r\n")
+	io.flush()
+	helloServerMessage = io.readline()
+
+	print('response ', helloServerMessage)
+
 	s.close()
-	
-	# decrypt ciphertext
-	key= dh[:16] # first 16 byes of the DH secret
-	print("key:",key.hex())
-	iv= bytes([0x0]) * 16 # iv=0
-	print("iv:",iv.hex())
-	cipher= AES.new(key,AES.MODE_CBC,iv)
-	plaintext= cipher.decrypt(ciphertext)
-	print("padded plaintext:",plaintext.hex())
-	
-	# remove padding
-	plaintext= unpad(plaintext)
-	print("unpad plaintext:",plaintext.hex())
-	print("message:",plaintext.decode())
+
+	exit()
 
